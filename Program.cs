0781cc332822e4ef1197460f894f691948e33fb8
Maks <maks-lug333@yandex.ru>
2023-02-09 20:10:17 +0900

    gitignor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("toto Hello, World!");
+﻿Console.Write("введите свое имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
